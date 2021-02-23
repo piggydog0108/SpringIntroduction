e9891b3eec12fc47025ece50cf8219a38f213818
tykang <fire0108@gmail.com>
2021-02-23 11:09:33 +0900

    firstCommit

---
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+	//https://twofootdog.tistory.com/57 참조
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+
+}
